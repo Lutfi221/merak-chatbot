b17d1474aea54db7cfb9acea622e8f62027eef20
Lutfi Azis <17lutfi21@gmail.com>
2021-09-09 12:58:54 +0700

    test: disable coverage collection

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  collectCoverage: true,
+  collectCoverage: false,
   collectCoverageFrom: ["src/**/*.ts", "src/**/*.js"],
   coverageThreshold: {
     global: {
