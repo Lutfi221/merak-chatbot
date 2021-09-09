28f2d60d7ad0eeaede5a12ba8c27434f2a5f4dff
Lutfi Azis <17lutfi21@gmail.com>
2021-09-09 20:03:07 +0700

    test: add tests for 'values' overlapping with 'links' and 'userInput'

--- src/chatbot/__tests__/inputs.test.ts
+++ src/chatbot/__tests__/inputs.test.ts
@@ -152,3 +152,121 @@ test("free user inputs", () => {
     freeLettersNoInvalidMessage: "cd",
   });
 });
+
+const inputLinksOverlaps: Data = {
+  pages: {
+    "/start": {
+      name: "initialChoice",
+      content: "values + links",
+      values: {
+        a: "a",
+        b: "b",
+      },
+      links: {
+        a: "/a",
+        b: "/b",
+      },
+    },
+    "/a": {
+      content: "a",
+    },
+    "/b": [
+      {
+        content: "b",
+      },
+      {
+        name: "number",
+        content: "values + userInput",
+        values: {
+          a: "1",
+          b: "2",
+          c: "3",
+        },
+        userInput: true,
+        userInputValidator: "^\\d+$",
+      },
+      {
+        name: "letter",
+        content: "values + links + userInput",
+        values: {
+          a: "a",
+          b: "b",
+          c: "c",
+        },
+        links: {
+          c: "/end",
+        },
+        userInput: true,
+        userInputValidator: "^[a-z]$",
+      },
+      {
+        // pause, so it doesn't immediately go back to "/start"
+        userInput: true,
+      },
+    ],
+    "/end": {
+      content: "end",
+    },
+  },
+};
+
+test("inputs overlaps", () => {
+  const chatbot = new Chatbot(inputLinksOverlaps);
+  const outputs: string[] = [];
+
+  chatbot.on("output", (msg) => outputs.push(msg));
+  chatbot.initialize();
+
+  chatbot.input("a");
+  expect(chatbot.storage.initialChoice).toBe("a");
+
+  chatbot.input("b");
+  expect(chatbot.storage.initialChoice).toBe("b");
+
+  chatbot.input("z");
+
+  chatbot.input("a");
+  expect(chatbot.storage.number).toBe("1");
+
+  chatbot.navigate("/b", 1);
+  chatbot.input("b");
+  expect(chatbot.storage.number).toBe("2");
+
+  chatbot.navigate("/b", 1);
+  chatbot.input("c");
+  expect(chatbot.storage.number).toBe("3");
+
+  chatbot.navigate("/b", 1);
+  chatbot.input("221");
+  expect(chatbot.storage.number).toBe("221");
+
+  chatbot.input("a");
+  expect(chatbot.storage.letter).toBe("a");
+
+  chatbot.navigate("/b", 2);
+  chatbot.input("b");
+  expect(chatbot.storage.letter).toBe("b");
+
+  chatbot.navigate("/b", 2);
+  chatbot.input("x");
+  expect(chatbot.storage.letter).toBe("x");
+
+  chatbot.navigate("/b", 2);
+  chatbot.input("c");
+  expect(chatbot.storage.letter).toBe("c");
+
+  expect(outputs).toEqual([
+    "values + links",
+    "a",
+    "values + links",
+    "b",
+    "values + userInput",
+    "values + userInput",
+    "values + links + userInput",
+    "values + links + userInput",
+    "values + links + userInput",
+    "values + links + userInput",
+    "end",
+    "values + links",
+  ]);
+});
